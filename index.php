e07c5ea36b323ee9799eb3642d3acf8e5f7d964f
Taskhgar <marc.lupi@protonmail.com>
2018-08-30 15:28:53 +0200

    création du formulaire de connexion dans la page index.php

--- index.php
+++ index.php
@@ -6,7 +6,37 @@
 		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	<body>
-		<?php
-		?>
+
+		<div class="page">
+
+			<div id="divConnexion">
+
+				<form id="formConnexion">
+
+					<div>
+						<h3>CONNEXION</h3>
+					</div>
+
+					<div id="inputUser">
+						<label for="userName">Utilisateur</label><br>
+						<input type="text" name="userName"><br><br>
+					</div>
+
+					<div id="inputPassword">
+						<label for="userassword">Mot de passe</label><br>
+						<input type="password" name="userPassword"><br><br>
+					</div>
+
+					<div id="inputSubmit">
+						<label for="userSubmit"> 	</label><br>
+						<input type="submit" name="userSubmit"><br><br>
+					</div>
+
+				</form>
+
+			</div>
+
+		</div>
+
 	</body>
 </html>
