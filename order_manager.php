53867f138ea78f3198661f56ddbfc4e92b19ae8f
nicolas <nicolas.lasportes@gmail.com>
2018-08-30 11:08:14 +0200

    initialisation des pages

---
+++ order_manager.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title>order_manager</title>
+	</head>
+	<body>
+
+	</body>
+</html>
---
+++ product_seller.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title>product_seller</title>
+	</head>
+	<body>
+
+	</body>
+</html>
