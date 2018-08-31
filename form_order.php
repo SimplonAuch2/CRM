2f4d3ea7f6f77c0cd4da6605bdaac0e84f23139d
dylangouzenne <dylan.gouzenne@gmail.com>
2018-08-31 10:02:21 +0200

    mise a jour langage interface en vers fr du formulaire de commande

--- form_order.php
+++ form_order.php
@@ -28,73 +28,63 @@
 <form action="" method="get" class="form_order">
 	
 	<div class="zone_order_products">
-		<label for="order_products">products</label>
+		<label for="order_products">Produits</label>
 		<input type="text" name="order_products" id="order_products" required>
 	</div>
 
 	<div class="zone_order_quantity">
-		<label for="order_quantity">quantity</label>
+		<label for="order_quantity">Quantités</label>
 		<input type="int" name="order_quantity" id="order_quantity" required>
 	</div>
 
 	<div class="zone_order_price">
-		<label for="order_price">price</label>
+		<label for="order_price">Prix</label>
 		<input type="money" name="order_price" id="order_price" required>
 	</div>
 
 	<div class="zone_order_reduction">
-		<label for="order_reduction">reduction</label>
+		<label for="order_reduction">Réductions</label>
 		<input type="int" name="order_reduction" id="order_reduction" required>
 	</div>
 
 	<div class="zone_order_postal_charges">
-		<label for="order_postal_charges">postal charge</label>
+		<label for="order_postal_charges">Frais de port</label>
 		<input type="money" name="order_postal_charges" id="order_postal_charges" required>
 	</div>
 
 	<div class="zone_order_totalprice">
-		<label for="order_totalprice">order_totalprice</label>
+		<label for="order_totalprice">Prix total</label>
 		<input type="money" name="order_totalprice" id="order_totalprice" required>
 	</div>
 
 	<div class="zone_order_taxes">
-		<label for="order_taxes">order_taxes</label>
+		<label for="order_taxes">TVA</label>
 		<input type="money" name="order_taxes" id="order_taxes" required>
 	</div>
 
 	<div class="zone_order_delivery_adresse">
-		<label for="order_delivery_adresse">delivery adresse</label>
+		<label for="order_delivery_adresse">Adresse de livraison</label>
 		<input type="text" name="order_delivery_adresse" id="order_delivery_adresse" required>
 	</div>
 
 	<div class="zone_order_billing_adresse">
-		<label for="order_billing_adresse">billing adresse</label>
+		<label for="order_billing_adresse">Adresse de facturation</label>
 		<input type="text" name="order_billing_adresse" id="order_billing_adresse" required>
 	</div>
 
-	<div class="zone_order_state">
-		<label for="order_date">order tate</label>
+	<div class="zone_order_date">
+		<label for="order_date">Date</label>
 		<input type="date" name="order_date" id="order_date" required>
-	</div>
-
-	<div class="zone_order_state">
-		<label for="order_state">order state</label>
-		<input type="text" name="order_state" id="order_state" required>
-	</div>
-
-	<div class="zone_order_delivery_date">
-		<label for="order_delivery_date">delivery date</label>
-		<input type="date" name="order_delivery_date" id="order_delivery_date" required>
 	</div>
 
 	<div class="zone_submit_order">
 		<label for="submit_order"></label>
-		<input type="submit" name="submit_order" id="submit_order" value="submit order">
+		<input type="submit" name="submit_order" id="submit_order" value="Valider commande">
 	</div>
 
 	<div class="zone_request a quote">
 		<label for="request_a_quote"></label>
-		<input type="submit" name="request_a_quote" id="request_a_quote" value="request a quote">
+		<input type="submit" name="request_a_quote" id="request_a_quote" value="Editer devis">
 	</div>
 
 </form>
