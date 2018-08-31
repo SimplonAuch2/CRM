45a801a73eb4173a23ee8cea6c39755b64eb49f0
dylangouzenne <dylan.gouzenne@gmail.com>
2018-08-31 15:31:12 +0200

    maj form et tableau commande

---
+++ form_purchase.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title>form_purchase</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+		<!-- liste champs table orders bdd :
+
+		order_id : auto_increment
+		order_products : varchar
+		order_quantity : int
+		order_price : money
+		order_reduction : int
+		order_postal_charges : money
+		order_totalprice : money
+		order_taxes : money
+		order_delivery_adresse : varchar
+		order_billing_adresse : varchar
+		order_date : date
+		order_state : varchar
+		order_delivery_date : date -->
+
+<h2>Form orders</h2>
+
+<form action="" method="get" class="form_order">
+	
+	<div class="zone_order_products">
+		<label for="order_products">Produits</label>
+		<input type="text" name="order_products" id="order_products" required>
+	</div>
+
+	<div class="zone_order_quantity">
+		<label for="order_quantity">Quantités</label>
+		<input type="int" name="order_quantity" id="order_quantity" required>
+	</div>
+
+	<div class="zone_order_price">
+		<label for="order_price">Prix</label>
+		<input type="money" name="order_price" id="order_price" required>
+	</div>
+
+	<div class="zone_order_reduction">
+		<label for="order_reduction">Réductions</label>
+		<input type="int" name="order_reduction" id="order_reduction" required>
+	</div>
+
+	<div class="zone_order_postal_charges">
+		<label for="order_postal_charges">Frais de port</label>
+		<input type="money" name="order_postal_charges" id="order_postal_charges" required>
+	</div>
+
+	<div class="zone_order_totalprice">
+		<label for="order_totalprice">Prix total</label>
+		<input type="money" name="order_totalprice" id="order_totalprice" required>
+	</div>
+
+	<div class="zone_order_taxes">
+		<label for="order_taxes">TVA</label>
+		<input type="money" name="order_taxes" id="order_taxes" required>
+	</div>
+
+	<div class="zone_order_delivery_adresse">
+		<label for="order_delivery_adresse">Adresse de livraison</label>
+		<input type="text" name="order_delivery_adresse" id="order_delivery_adresse" required>
+	</div>
+
+	<div class="zone_order_billing_adresse">
+		<label for="order_billing_adresse">Adresse de facturation</label>
+		<input type="text" name="order_billing_adresse" id="order_billing_adresse" required>
+	</div>
+
+	<div class="zone_order_date">
+		<label for="order_date">Date</label>
+		<input type="date" name="order_date" id="order_date" required>
+	</div>
+
+	<div class="zone_submit_order">
+		<label for="submit_order"></label>
+		<input type="submit" name="submit_order" id="submit_order" value="Valider commande">
+	</div>
+
+	<div class="zone_request a quote">
+		<label for="request_a_quote"></label>
+		<input type="submit" name="request_a_quote" id="request_a_quote" value="Editer devis">
+	</div>
+
+</form>
+
+</body>
+</html>
---
+++ purchase_seller.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title>purchase_seller</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div id="contentPurchaseSeller">
+			<div id="purchase">
+				<table class="tablePurchase">
+
+					<thead>
+
+						<th>Client</th>
+						<th>Produits</th>
+						<th>Quantités</th>
+						<th>Prix</th>
+						<th>Réductions</th>
+						<th>Frais de port</th>
+						<th>Prix total</th>
+						<th>TVA</th>
+						<th>Adresse de livraison</th>
+						<th>Adresse de facturation</th>
+						<th>Date</th>
+
+					</thead>
+
+					<?php
+
+					// CONNEXION TO THE DATABASE
+					// CONNEXION WITH THE FILE connect.php
+					try
+					{
+						$bdd = new PDO('mysql:host=localhost;dbname=crmV2;charset=utf8', 'user_bdd', 'password_bdd');
+					}
+					catch(Exception $e)
+					{
+						die('Erreur : '.$e->getMessage());
+					}
+
+					// on récupère et on affiche le contenu de la base de données
+					$req = $bdd->query('SELECT * FROM purchase');
+					while ($donnees = $req->fetch())
+						{ echo
+							'<tr><td>' . $donnees['customerId'] . '</td>' .
+							'<td>' . $donnees['purchaseProducts'] . '</td>' .
+							'<td>' . $donnees['purchaseQuantity'] . '</td>' .
+							'<td>' . $donnees['purchasePrice'] . '</td>' .
+							'<td>' . $donnees['purchaseReduction'] . '</td>' .
+							'<td>' . $donnees['purchasePostalCharges'] . '</td>' .
+							'<td>' . $donnees['purchaseTotalprice'] . '</td>' .
+							'<td>' . $donnees['purchaseTaxes'] . '</td>' .
+							'<td>' . $donnees['purchaseDeliveryAddress'] . '</td>' .
+							'<td>' . $donnees['purchaseBillingAddress'] . '</td>' .
+							'<td>' . $donnees['purchaseDate'] . '</td></tr>';
+						}
+					?>
+
+				</table>
+
+				<button id="buttonOK" class="button" type="button">OK</button>
+
+			</div>
+	</body>
+</html>
